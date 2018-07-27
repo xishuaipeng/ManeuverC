fb509dd376d2d417c5ba8700539b184b6ec81bf4
xishuaipeng <xishuaipeng@gmail.com>
2018-07-26 22:59:47 -0400

    batchnorm traing stage
    solve batchnorm traing stage

--- MR/Encoder/Encoder.py
+++ MR/Encoder/Encoder.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 import tensorflow.contrib.slim.nets as slimNet
+from tensorflow.contrib.layers.python.layers import layers as layers_lib
 
 class Encoder:
     def __init__(self):
@@ -60,11 +61,14 @@ class Inception_v3(Encoder):
  
     def build(self, input_shape = [None, 299, 299, 3],n_class=1 ):
         inputs = tf.placeholder(tf.float32,shape = input_shape, name='Inputs')
-        arg_scope = slimNet.inception.inception_v3_arg_scope(batch_norm_decay=0.9997, batch_norm_epsilon=0.001,use_fused_batchnorm=True)
+        arg_scope = slimNet.inception.inception_v3_arg_scope()
+        is_train = tf.placeholder(tf.bool,shape=[], name='is_train_BN')
         with slim.arg_scope(arg_scope):
-            _, end_points = slimNet.inception.inception_v3_base(inputs)
+            with slim.arg_scope([layers_lib.batch_norm, layers_lib.dropout], is_training =is_train)
+                _, end_points = slimNet.inception.inception_v3_base(inputs)
         self.end_points = end_points
         self.end_points['Inputs'] = inputs
+        self.end_points['is_train']  = is_train
 
     
     def load_model(self, sess, model_path = '../../Model/inception_v3.ckpt'):
@@ -86,4 +90,4 @@ class Inception_v3(Encoder):
     def back_bone(self,input_shape ):
         self.build(input_shape)
         # [n,w,h,c] = self.end_points['Mixed_7c']
-        return self.end_points['Inputs'], self.end_points['Mixed_7c']
+        return self.end_points['Inputs'], self.end_points['Mixed_7c'], self.end_points['is_train']
